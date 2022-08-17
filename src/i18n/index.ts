f5017c4ceb1d3fdaffdf740142a654e7ce376ead
Lasse <lassetange@gmail.com>
2022-08-17 16:07:58 +0200

    Adding all locales

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,6 +2,9 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from './locales/en.json';
 import de from './locales/de.json';
+import dk from './locales/dk.json';
+import deAT from './locales/de_at.json';
+import frFR from './locales/fr-fr.json';
 
 // the translations
 // (tip move them in a JSON file and import them,
@@ -9,6 +12,9 @@ import de from './locales/de.json';
 const resources = {
   en: { translation: en },
   de: { translation: de },
+  dk: { translation: dk },
+  at: { translation: deAT },
+  frFR: { translation: frFR },
 };
 
 i18n
