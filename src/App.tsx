6ae7f7219bfda86472739b81edf0ad9c2ab11dc1
Lasse <lassetange@gmail.com>
2022-08-17 16:00:39 +0200

    Updating <Trans/> and adding German

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,13 @@ import { useTranslation } from 'react-i18next';
 import { Trans } from 'react-i18next';
 
 function App() {
-  const { t, i18n } = useTranslation();
-  console.log({ i18n });
-
+  const { t } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <Trans>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </Trans>
+          <Trans i18nKey="frontpage.title" t={t} components={[<code />]} />
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           {t('frontpage.learn')}
