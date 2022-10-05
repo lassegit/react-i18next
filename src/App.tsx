7347c8d3f9a7951e2659164afd08424ab07a1b65
Lasse <lassetange@gmail.com>
2022-10-05 13:02:27 +0200

    Update translations

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation, Trans } from 'react-i18next';
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+function App() {
+  const { t } = useTranslation();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <Trans i18nKey="frontpage.title" t={t} components={[<code />]} />
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          {t('frontpage.learn')}
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,19 +1,16 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from './locales/en.json';
+import de from './locales/de.json';
+import dk from './locales/dk.json';
 
-// the translations
+// The translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
-  en: {
-    translation: en,
-  },
-  // fr: {
-  //   translation: {
-  //     'Welcome to React': 'Bienvenue à React et react-i18next',
-  //   },
-  // },
+  en: { translation: en },
+  de: { translation: de },
+  dk: { translation: dk },
 };
 
 i18n
